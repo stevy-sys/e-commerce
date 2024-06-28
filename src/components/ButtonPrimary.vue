96bc15649fa21af13504f0f757409ac444413a5e
stevy-sys <stevyralambomanana@mail.com>
2024-06-28 15:43:25 +0200

    init color

---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ion-button>Sign up</ion-button>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { IonButton } from '@ionic/vue';
+//   import { defineProps } from 'vue'
+  const props = defineProps({
+    color:{
+        Type: String,
+        Default: 'primary'
+    }
+  })
+  </script>
+
+<style lang="scss" scoped>
+  
+</style>
+  
--- src/views/Composant.vue
+++ src/views/Composant.vue
@@ -21,15 +21,19 @@
 
 
         <!-- stevy -->
-
+        <ButtonPrimary color="signup" />
         <!-- fin stevy -->
-        <!-- <ExploreContainer name="Tab 1 page" /> -->
       </ion-content>
     </ion-page>
   </template>
   
   <script setup lang="ts">
   import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-  import ExploreContainer from '@/components/ExploreContainer.vue';
+
+  import ButtonPrimary from '@/components/ButtonPrimary.vue';
   </script>
+
+<style scoped>
+  
+</style>
   
